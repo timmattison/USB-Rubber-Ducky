80ec93c83ea6783dc518c38ed22294603c4cfe34
Tim Mattison <tim@mattison.org>
2014-07-25 17:27:30 -0400

    Simplified script to run the original encoder

--- run-original-encoder.sh
+++ run-original-encoder.sh
@@ -2,4 +2,4 @@
 
 FILE=$1
 
-java -jar ./out/artifacts/old_USB_Rubber_Ducky_jar/USB-Rubber-Ducky.jar -i `find . -name "$FILE.txt"` -o Encoder/test-files/outputs/$FILE.bin
+java -jar ./out/artifacts/old_USB_Rubber_Ducky_jar/USB-Rubber-Ducky.jar -i Encoder/test-files/inputs/$FILE.txt -o Encoder/test-files/outputs/$FILE.bin
