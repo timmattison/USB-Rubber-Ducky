25d45aadb182250fd17813cc59cae3f3c776062f
Tim Mattison <tim@mattison.org>
2014-07-28 19:03:21 -0400

    Added restart-prank

--- Encoder/src/test/java/com/timmattison/hacking/usbrubberducky/parsers/IntegrationTest.java
+++ Encoder/src/test/java/com/timmattison/hacking/usbrubberducky/parsers/IntegrationTest.java
@@ -134,6 +134,13 @@ public class IntegrationTest {
         testFile(filename);
     }
 
+    @Test
+    public void testRestartPrank() throws IOException {
+        String filename = "restart-prank";
+
+        testFile(filename);
+    }
+
     private void testFile(String filename) throws IOException {
         String[] inputFile;
         byte[] outputFile;
