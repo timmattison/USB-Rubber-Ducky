079849056fff861ea0661d359a0c813d414678ff
Tim Mattison <tim@mattison.org>
2014-07-30 09:56:12 -0400

    Comments

--- Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/preprocessors/LegacyPreprocessor.java
+++ Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/preprocessors/LegacyPreprocessor.java
@@ -8,9 +8,18 @@ package com.timmattison.hacking.usbrubberducky.preprocessors;
 public class LegacyPreprocessor implements Preprocessor {
     @Override
     public String preprocess(String input) {
+        /**
+         * "CTRL-SHIFT" needs to be translated to "CTRL SHIFT" because we stack up the modifiers automatically rather
+         * than directly supporting a new code that is both values combined
+         */
         input = input.replaceAll("^CTRL-SHIFT", "CTRL SHIFT");
         input = input.replaceAll("^CONTROL-SHIFT", "CONTROL SHIFT");
+
+        /**
+         * We always treat "DEFAULTDELAY" as "DEFAULT_DELAY".  No reason to have another code for that.
+         */
         input = input.replaceAll("^DEFAULTDELAY", "DEFAULT_DELAY");
+
         return input;
     }
 }
