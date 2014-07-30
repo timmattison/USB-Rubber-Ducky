5a50fda92917518ede3087cf61ad004520af3653
Tim Mattison <tim@mattison.org>
2014-07-30 10:02:04 -0400

    Comments

--- Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/translation/keyboards/AbstractKeyboardCodes.java
+++ Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/translation/keyboards/AbstractKeyboardCodes.java
@@ -9,6 +9,12 @@ import java.util.Map;
  * Created by timmattison on 12/9/13.
  */
 public abstract class AbstractKeyboardCodes {
+    /**
+     * Adds keyboard codes to a map
+     *
+     * @param keyboardCodeValues the codes to add to the map
+     * @param keyboardCodes      the map to which the codes are added
+     */
     protected void addKeyboardValues(ReturnsKeyboardCode[] keyboardCodeValues, Map<String, KeyboardCode> keyboardCodes) {
         for (ReturnsKeyboardCode keyboardCodeValue : keyboardCodeValues) {
             keyboardCodes.put(keyboardCodeValue.getValue().getStringToMatch(), keyboardCodeValue.getValue());
