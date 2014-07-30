795cdaf2568395793776d3e2a3e64e945ee83cb1
Tim Mattison <tim@mattison.org>
2014-07-30 10:12:43 -0400

    Comments

--- Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/parsers/regex/NopInstructionParser.java
+++ Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/parsers/regex/NopInstructionParser.java
@@ -13,6 +13,7 @@ import java.util.List;
  * Time: 11:32 AM
  * To change this template use File | Settings | File Templates.
  */
+// TODO: Rename this RemInstructionParser
 public class NopInstructionParser extends RegexAbstractInstructionParser<NopInstruction> {
     private static final String name = "REM ";
     private static final String matchingRegex = "^" + name;
