57823ff35f5abfea1233dcdf59dac325a5a2e1ea
Tim Mattison <tim@mattison.org>
2014-07-31 18:34:40 -0400

    Created custom exceptions

---
+++ Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/exceptions/ModifierCollisionException.java
@@ -0,0 +1,20 @@
+package com.timmattison.hacking.usbrubberducky.exceptions;
+
+import com.timmattison.hacking.usbrubberducky.translation.codes.KeyboardCode;
+
+import java.util.Stack;
+
+/**
+ * Created by timmattison on 7/31/14.
+ */
+public class ModifierCollisionException extends Throwable {
+    private final Stack<KeyboardCode> keyboardCodeStack;
+
+    public ModifierCollisionException(Stack<KeyboardCode> keyboardCodeStack) {
+        this.keyboardCodeStack = keyboardCodeStack;
+    }
+
+    public Stack<KeyboardCode> getKeyboardCodeStack() {
+        return keyboardCodeStack;
+    }
+}
