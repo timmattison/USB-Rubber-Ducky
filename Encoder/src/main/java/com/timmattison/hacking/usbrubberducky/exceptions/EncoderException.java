1b8570c64b43601bc62259f87ac63dd027b45e0d
Tim Mattison <tim@mattison.org>
2014-07-31 18:43:55 -0400

    EncoderException now extends Exception

--- Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/exceptions/EncoderException.java
+++ Encoder/src/main/java/com/timmattison/hacking/usbrubberducky/exceptions/EncoderException.java
@@ -3,5 +3,5 @@ package com.timmattison.hacking.usbrubberducky.exceptions;
 /**
  * Created by timmattison on 7/31/14.
  */
-public class EncoderException extends Throwable {
+public class EncoderException extends Exception {
 }
